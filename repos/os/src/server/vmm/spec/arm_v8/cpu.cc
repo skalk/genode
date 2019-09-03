02a1ccaca54f832462e19b9595a66bb809274197
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-09-03 16:29:27 +0200

    os: introduce vmm component for ARMv8

---
+++ repos/os/src/server/vmm/spec/arm_v8/cpu.cc
@@ -0,0 +1,316 @@
+/*
+ * \brief  VMM cpu object
+ * \author Stefan Kalkowski
+ * \date   2019-07-18
+ */
+
+/*
+ * Copyright (C) 2019 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+#include <cpu.h>
+#include <vm.h>
+
+using Vmm::Cpu;
+
+Cpu::System_register::Iss::access_t
+Cpu::System_register::Iss::value(unsigned op0, unsigned crn, unsigned op1,
+                                 unsigned crm, unsigned op2)
+{
+	access_t v = 0;
+	Crn::set(v, crn);
+	Crm::set(v, crm);
+	Opcode0::set(v, op0);
+	Opcode1::set(v, op1);
+	Opcode2::set(v, op2);
+	return v;
+};
+
+
+Cpu::System_register::Iss::access_t
+Cpu::System_register::Iss::mask_encoding(access_t v)
+{
+	return Crm::masked(v) |
+	       Crn::masked(v) |
+	       Opcode1::masked(v) |
+	       Opcode2::masked(v) |
+	       Opcode0::masked(v);
+}
+
+
+Cpu::System_register::System_register(unsigned         op0,
+                                      unsigned         crn,
+                                      unsigned         op1,
+                                      unsigned         crm,
+                                      unsigned         op2,
+                                      const char     * name,
+                                      bool             writeable,
+                                      Genode::addr_t   v,
+                                      Genode::Avl_tree<System_register> & tree)
+: _encoding(Iss::value(op0, crn, op1, crm, op2)),
+  _name(name),
+  _writeable(writeable),
+  _value(v)
+{
+	tree.insert(this);
+}
+
+
+Genode::addr_t Cpu::Ccsidr::read() const
+{
+	struct Clidr : Genode::Register<32>
+	{
+		enum Cache_entry {
+			NO_CACHE,
+			INSTRUCTION_CACHE_ONLY,
+			DATA_CACHE_ONLY,
+			SEPARATE_CACHE,
+			UNIFIED_CACHE
+		};
+
+		static unsigned level(unsigned l, access_t reg) {
+			return (reg >> l*3) & 0b111; }
+	};
+
+	struct Csselr : Genode::Register<32>
+	{
+		struct Instr : Bitfield<0, 1> {};
+		struct Level : Bitfield<1, 4> {};
+	};
+
+	enum { INVALID = 0xffffffff };
+
+	unsigned level = Csselr::Level::get(csselr.read());
+	bool     instr = Csselr::Instr::get(csselr.read());
+
+	if (level > 6) {
+		Genode::warning("Invalid Csselr value!");
+		return INVALID;
+	}
+
+	unsigned ce = Clidr::level(level, state.clidr_el1);
+
+	if (ce == Clidr::NO_CACHE ||
+	    (ce == Clidr::DATA_CACHE_ONLY && instr)) {
+		Genode::warning("Invalid Csselr value!");
+		return INVALID;
+	}
+
+	if (ce == Clidr::INSTRUCTION_CACHE_ONLY ||
+	    (ce == Clidr::SEPARATE_CACHE && instr)) {
+		Genode::log("Return Ccsidr instr value ", state.ccsidr_inst_el1[level]);
+		return state.ccsidr_inst_el1[level];
+	}
+
+	Genode::log("Return Ccsidr value ", state.ccsidr_data_el1[level]);
+	return state.ccsidr_data_el1[level];
+}
+
+
+Genode::addr_t Cpu::Ctr_el0::read() const
+{
+	Genode::addr_t ret;
+	asm volatile("mrs %0, ctr_el0" : "=r" (ret));
+	return ret;
+}
+
+
+bool Cpu::_handle_sys_reg()
+{
+	using Iss = System_register::Iss;
+
+	Iss::access_t v = _state.esr_el2;
+	System_register * reg = _reg_tree.first();
+	if (reg) reg = reg->find_by_encoding(Iss::mask_encoding(v));
+
+	if (!reg) {
+		Genode::error("unknown system register access @ ip=", (void*)_state.ip, ":");
+		Genode::error(Iss::Direction::get(v) ? "read" : "write",
+		              ": "
+		              "op0=",  Iss::Opcode0::get(v), " "
+		              "op1=",  Iss::Opcode1::get(v), " "
+		              "r",    Iss::Register::get(v),   " "
+		              "crn=",  Iss::Crn::get(v),        " "
+		              "crm=",  Iss::Crm::get(v), " ",
+		              "op2=",  Iss::Opcode2::get(v));
+		return false;
+	}
+
+	if (Iss::Direction::get(v)) { /* read access  */
+		_state.r[Iss::Register::get(v)] = reg->read();
+	} else {                      /* write access */
+		if (!reg->writeable()) {
+			Genode::error("writing to system register ",
+			              reg->name(), " not allowed!");
+			return false;
+		}
+		reg->write(_state.r[Iss::Register::get(v)]);
+	}
+	_state.ip += sizeof(Genode::uint32_t);
+	return true;
+}
+
+
+void Cpu::_handle_wfi()
+{
+	if (_state.esr_el2 & 1)
+		throw Exception("WFE not implemented yet");
+
+	_active = false;
+	_timer.schedule_timeout();
+	_state.ip += sizeof(Genode::uint32_t);
+}
+
+
+void Cpu::_handle_brk()
+{
+	Genode::uint64_t offset = 0x0;
+	if (!(_state.pstate & 0b100)) {
+		offset = 0x400;
+	} else if (_state.pstate & 0b1) {
+		offset = 0x200;
+	}
+	_state.esr_el1  = _state.esr_el2;
+	_state.spsr_el1 = _state.pstate;
+	_state.elr_el1  = _state.ip;
+	_state.ip       = _state.vbar_el1 + offset;
+	_state.pstate   = 0b1111000101;
+}
+
+
+void Cpu::_handle_sync()
+{
+	/* check device number*/
+	switch (Esr::Ec::get(_state.esr_el2)) {
+	case Esr::Ec::HVC:
+		_vm.handle_hyper_call();
+		break;
+	case Esr::Ec::MRS_MSR:
+		_handle_sys_reg();
+		break;
+	case Esr::Ec::DA:
+		_vm.handle_data_abort();
+		break;
+	case Esr::Ec::WFI:
+		_handle_wfi();
+		return;
+	case Esr::Ec::BRK:
+		_handle_brk();
+		return;
+	default:
+		throw Exception("Unknown trap: %x",
+		                Esr::Ec::get(_state.esr_el2));
+	};
+}
+
+
+void Cpu::_handle_irq()
+{
+	enum { /* FIXME */ VT_TIMER_IRQ = 27 };
+	switch (_state.irqs.last_irq) {
+	case VT_TIMER_IRQ:
+		_timer.handle_irq();
+		break;
+	default:
+		_gic.handle_irq();
+	};
+}
+
+
+void Cpu::_update_state()
+{
+	if (!_gic.pending_irq()) return;
+	_active = true;
+	_timer.cancel_timeout();
+}
+
+
+void Cpu::dump()
+{
+	using namespace Genode;
+
+	auto lambda = [] (addr_t exc) {
+		switch (exc) {
+		case AARCH64_SYNC:   return "aarch64 sync";
+		case AARCH64_IRQ:    return "aarch64 irq";
+		case AARCH64_FIQ:    return "aarch64 fiq";
+		case AARCH64_SERROR: return "aarch64 serr";
+		case AARCH32_SYNC:   return "aarch32 sync";
+		case AARCH32_IRQ:    return "aarch32 irq";
+		case AARCH32_FIQ:    return "aarch32 fiq";
+		case AARCH32_SERROR: return "aarch32 serr";
+		default:             return "unknown";
+		};
+	};
+
+	log("VM state (", _active ? "active" : "inactive", ") :");
+	for (unsigned i = 0; i < 31; i++) {
+		log("  r", i, "         = ",
+		    Hex(_state.r[i], Hex::PREFIX, Hex::PAD));
+	}
+	log("  sp         = ", Hex(_state.sp,      Hex::PREFIX, Hex::PAD));
+	log("  ip         = ", Hex(_state.ip,      Hex::PREFIX, Hex::PAD));
+	log("  sp_el1     = ", Hex(_state.sp_el1,  Hex::PREFIX, Hex::PAD));
+	log("  elr_el1    = ", Hex(_state.elr_el1, Hex::PREFIX, Hex::PAD));
+	log("  pstate     = ", Hex(_state.pstate,  Hex::PREFIX, Hex::PAD));
+	log("  exception  = ", _state.exception_type, " (",
+	                       lambda(_state.exception_type), ")");
+	log("  esr_el2    = ", Hex(_state.esr_el2, Hex::PREFIX, Hex::PAD));
+	_timer.dump();
+}
+
+
+Cpu::Cpu(Vm                      & vm,
+         Genode::Vm_connection   & vm_session,
+         Mmio_bus                & bus,
+         Gic                     & gic,
+         Genode::Env             & env,
+         Genode::Heap            & heap,
+         Genode::Vm_handler_base & handler,
+         Genode::addr_t            ip,
+         Genode::addr_t            dtb)
+: _vm(vm),
+  _vm_session(vm_session),
+  _heap(heap),
+  _vcpu_id(_vm_session.create_vcpu(heap, env, handler)),
+  _state(*((State*)env.rm().attach(_vm_session.cpu_state(_vcpu_id)))),
+	//                op0, crn, op1, crm, op2, writeable, reset value
+  _sr_id_aa64afr0_el1 (3, 0, 0, 5, 4, "ID_AA64AFR0_EL1",  false, 0x0, _reg_tree),
+  _sr_id_aa64afr1_el1 (3, 0, 0, 5, 5, "ID_AA64AFR1_EL1",  false, 0x0, _reg_tree),
+  _sr_id_aa64dfr0_el1 (3, 0, 0, 5, 0, "ID_AA64DFR0_EL1",  false, 0x6, _reg_tree),
+  _sr_id_aa64dfr1_el1 (3, 0, 0, 5, 1, "ID_AA64DFR1_EL1",  false, 0x0, _reg_tree),
+  _sr_id_aa64isar0_el1(3, 0, 0, 6, 0, "ID_AA64ISAR0_EL1", false, _state.id_aa64isar0_el1, _reg_tree),
+  _sr_id_aa64isar1_el1(3, 0, 0, 6, 1, "ID_AA64ISAR1_EL1", false, _state.id_aa64isar1_el1, _reg_tree),
+  _sr_id_aa64mmfr0_el1(3, 0, 0, 7, 0, "ID_AA64MMFR0_EL1", false, _state.id_aa64mmfr0_el1, _reg_tree),
+  _sr_id_aa64mmfr1_el1(3, 0, 0, 7, 1, "ID_AA64MMFR1_EL1", false, _state.id_aa64mmfr1_el1, _reg_tree),
+  _sr_id_aa64mmfr2_el1(3, 0, 0, 7, 2, "ID_AA64MMFR2_EL1", false, _state.id_aa64mmfr2_el1, _reg_tree),
+  _sr_id_aa64pfr0_el1 (_state.id_aa64pfr0_el1, _reg_tree),
+  _sr_id_aa64pfr1_el1 (3, 0, 0, 4, 1, "ID_AA64PFR1_EL1",  false, 0x0, _reg_tree),
+  _sr_id_aa64zfr0_el1 (3, 0, 0, 4, 4, "ID_AA64ZFR0_EL1",  false, 0x0, _reg_tree),
+  _sr_aidr_el1        (3, 0, 1, 0, 7, "AIDR_EL1",         false, 0x0, _reg_tree),
+  _sr_revidr_el1      (3, 0, 0, 0, 6, "REVIDR_EL1",       false, 0x0, _reg_tree),
+
+  _sr_clidr_el1       (3, 0, 1, 0, 1, "CLIDR_EL1",        false, _state.clidr_el1,        _reg_tree),
+  _sr_csselr_el1      (3, 0, 2, 0, 0, "CSSELR_EL1",       true,  0x0, _reg_tree),
+  _sr_ctr_el0         (_reg_tree),
+  _sr_ccsidr_el1      (_sr_csselr_el1, _state, _reg_tree),
+
+  //_sr_pmccfiltr_el0   (3, 14, 3, 15, 7, "PMCCFILTR_EL0",  true,  0x0,                     _reg_tree),
+  _sr_pmuserenr_el0   (3, 9, 3, 14, 0,  "PMUSEREN_EL0",   true,  0x0, _reg_tree),
+  _sr_dbgbcr0         (2, 0, 0, 0, 5, "DBGBCR_EL1",       true,  0x0, _reg_tree),
+  _sr_dbgbvr0         (2, 0, 0, 0, 4, "DBGBVR_EL1",       true,  0x0, _reg_tree),
+  _sr_dbgwcr0         (2, 0, 0, 0, 7, "DBGWCR_EL1",       true,  0x0, _reg_tree),
+  _sr_dbgwvr0         (2, 0, 0, 0, 6, "DBGWVR_EL1",       true,  0x0, _reg_tree),
+  _sr_mdscr           (2, 0, 0, 2, 2, "MDSCR_EL1",        true,  0x0, _reg_tree),
+  _sr_osdlr           (2, 1, 0, 3, 4, "OSDLR_EL1",        true,  0x0, _reg_tree),
+  _sr_oslar           (2, 1, 0, 0, 4, "OSLAR_EL1",        true,  0x0, _reg_tree),
+
+  _gic(*this, gic, bus),
+  _timer(env, _gic.irq(27), *this)
+{
+	_state.r[0]   = dtb;
+	_state.ip     = ip;
+	_state.pstate = 0b1111000101; /* el1 mode and IRQs disabled */
+}
---
+++ repos/os/src/server/vmm/spec/arm_v8/cpu.h
@@ -0,0 +1,327 @@
+/*
+ * \brief  VMM cpu object
+ * \author Stefan Kalkowski
+ * \date   2019-07-18
+ */
+
+/*
+ * Copyright (C) 2019 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _SRC__SERVER__VMM__CPU_H_
+#define _SRC__SERVER__VMM__CPU_H_
+
+#include <exception.h>
+#include <generic_timer.h>
+
+#include <base/env.h>
+#include <base/heap.h>
+#include <cpu/vm_state_virtualization.h>
+#include <util/mmio.h>
+#include <vm_session/connection.h>
+
+namespace Vmm {
+	class Vm;
+	class Cpu;
+}
+
+class Vmm::Cpu
+{
+	public:
+
+		struct Esr : Genode::Register<32>
+		{
+			struct Ec : Bitfield<26, 6>
+			{
+				enum {
+					WFI     = 0x1,
+					HVC     = 0x16,
+					MRS_MSR = 0x18,
+					DA      = 0x24,
+					BRK     = 0x3c
+				};
+			};
+		};
+
+		using State = Genode::Vm_state;
+
+		enum Exception_type {
+			AARCH64_SYNC   = 0x400,
+			AARCH64_IRQ    = 0x480,
+			AARCH64_FIQ    = 0x500,
+			AARCH64_SERROR = 0x580,
+			AARCH32_SYNC   = 0x600,
+			AARCH32_IRQ    = 0x680,
+			AARCH32_FIQ    = 0x700,
+			AARCH32_SERROR = 0x780,
+			NO_EXCEPTION   = 0xffff
+		};
+
+		class System_register : public Genode::Avl_node<System_register>
+		{
+			private:
+
+				const Esr::access_t       _encoding;
+				const char               *_name;
+				const bool                _writeable;
+				Genode::uint64_t          _value;
+
+			public:
+
+				struct Iss : Esr
+				{
+					struct Direction : Bitfield<0,  1> {};
+					struct Crm       : Bitfield<1,  4> {};
+					struct Register  : Bitfield<5,  4> {};
+					struct Crn       : Bitfield<10, 4> {};
+					struct Opcode1   : Bitfield<14, 3> {};
+					struct Opcode2   : Bitfield<17, 3> {};
+					struct Opcode0   : Bitfield<20, 2> {};
+
+					static access_t value(unsigned op0,
+					                      unsigned crn,
+					                      unsigned op1,
+					                      unsigned crm,
+					                      unsigned op2);
+
+					static access_t mask_encoding(access_t v);
+				};
+
+				System_register(unsigned         op0,
+				                unsigned         crn,
+				                unsigned         op1,
+				                unsigned         crm,
+				                unsigned         op2,
+				                const char     * name,
+				                bool             writeable,
+				                Genode::addr_t   v,
+				                Genode::Avl_tree<System_register> & tree);
+
+				const char * name()       const { return _name;      }
+				const bool   writeable()  const { return _writeable; }
+
+				System_register * find_by_encoding(Iss::access_t e)
+				{
+					if (e == _encoding) return this;
+
+					System_register * r =
+						Avl_node<System_register>::child(e > _encoding);
+					return r ? r->find_by_encoding(e) : nullptr;
+				}
+
+				virtual void write(Genode::addr_t v) {
+					_value = (Genode::uint32_t)v; }
+
+				virtual Genode::addr_t read() const {
+					return (Genode::addr_t)(_value); }
+
+
+				/************************
+				 ** Avl node interface **
+				 ************************/
+
+				bool higher(System_register *r) {
+					return (r->_encoding > _encoding); }
+		};
+
+		class Id_aa64pfr0 : public System_register,
+		                    public Genode::Register<64>
+		{
+			private:
+
+				struct El0 : Bitfield<0,  4> { enum { AARCH64_ONLY = 1 }; };
+				struct El1 : Bitfield<4,  4> { enum { AARCH64_ONLY = 1 }; };
+				struct El2 : Bitfield<8,  4> { enum { NOT_IMPLEMENTED  }; };
+				struct El3 : Bitfield<12, 4> { enum { NOT_IMPLEMENTED  }; };
+				struct Ras : Bitfield<28, 4> { enum { NOT_IMPLEMENTED  }; };
+				struct Sve : Bitfield<32, 4> { enum { NOT_IMPLEMENTED  }; };
+
+				access_t _reset_value(access_t orig)
+				{
+					El0::set(orig, El0::AARCH64_ONLY);
+					El1::set(orig, El1::AARCH64_ONLY);
+					El2::set(orig, El2::NOT_IMPLEMENTED);
+					El3::set(orig, El3::NOT_IMPLEMENTED);
+					Ras::set(orig, Ras::NOT_IMPLEMENTED);
+					Sve::set(orig, Sve::NOT_IMPLEMENTED);
+					return orig;
+				}
+
+			public:
+
+				Id_aa64pfr0(Genode::uint64_t id_aa64pfr0,
+				            Genode::Avl_tree<System_register> & tree)
+				: System_register(3, 0, 0, 4, 0, "ID_AA64PFR0_EL1", false,
+				                  _reset_value(id_aa64pfr0), tree) {}
+		};
+
+		struct Ccsidr : System_register
+		{
+			System_register & csselr;
+			State           & state;
+
+			Ccsidr(System_register &csselr,
+			       State & state,
+			       Genode::Avl_tree<System_register> & tree)
+			: System_register(3, 0, 1, 0, 0, "CCSIDR_EL1", false, 0x0, tree),
+			  csselr(csselr), state(state) {}
+
+			virtual Genode::addr_t read() const override;
+		};
+
+		struct Ctr_el0 : System_register
+		{
+			Ctr_el0(Genode::Avl_tree<System_register> & tree)
+			: System_register(3, 0, 3, 0, 1, "CTR_EL0", false, 0x0, tree) {}
+
+			virtual Genode::addr_t read() const override;
+		};
+
+	private:
+
+		unsigned                          _cpu_id;
+		Vm                              & _vm;
+		Genode::Vm_connection           & _vm_session;
+		Genode::Heap                    & _heap;
+		Genode::Vm_session::Vcpu_id       _vcpu_id;
+		State                           & _state;
+		bool                              _active { true };
+		Genode::Avl_tree<System_register> _reg_tree;
+
+		/******************************
+		 ** Identification registers **
+		 ******************************/
+
+		System_register                   _sr_id_aa64afr0_el1;
+		System_register                   _sr_id_aa64afr1_el1;
+		System_register                   _sr_id_aa64dfr0_el1;
+		System_register                   _sr_id_aa64dfr1_el1;
+		System_register                   _sr_id_aa64isar0_el1;
+		System_register                   _sr_id_aa64isar1_el1;
+		System_register                   _sr_id_aa64mmfr0_el1;
+		System_register                   _sr_id_aa64mmfr1_el1;
+		System_register                   _sr_id_aa64mmfr2_el1;
+		Id_aa64pfr0                       _sr_id_aa64pfr0_el1;
+		System_register                   _sr_id_aa64pfr1_el1;
+		System_register                   _sr_id_aa64zfr0_el1;
+		System_register                   _sr_aidr_el1;
+		System_register                   _sr_revidr_el1;
+
+		/*********************
+		 ** Cache registers **
+		 *********************/
+
+		System_register                   _sr_clidr_el1;
+		System_register                   _sr_csselr_el1;
+		Ctr_el0                           _sr_ctr_el0;
+		Ccsidr                            _sr_ccsidr_el1;
+
+		/***********************************
+		 ** Performance monitor registers **
+		 ***********************************/
+
+		System_register                   _sr_pmuserenr_el0;
+
+		/*****************************
+		 ** Debug monitor registers **
+		 *****************************/
+
+		System_register                   _sr_dbgbcr0;
+		System_register                   _sr_dbgbvr0;
+		System_register                   _sr_dbgwcr0;
+		System_register                   _sr_dbgwvr0;
+		System_register                   _sr_mdscr;
+		System_register                   _sr_osdlr;
+		System_register                   _sr_oslar;
+
+		/***********************
+		 ** Local peripherals **
+		 ***********************/
+
+		Gic::Gicd_banked                  _gic;
+		Generic_timer                     _timer;
+
+		bool _handle_sys_reg();
+		void _handle_brk();
+		void _handle_wfi();
+		void _handle_sync();
+		void _handle_irq();
+		void _update_state();
+
+	public:
+
+		Cpu(Vm                      & vm,
+		    Genode::Vm_connection   & vm_session,
+		    Mmio_bus                & bus,
+		    Gic                     & gic,
+		    Genode::Env             & env,
+		    Genode::Heap            & heap,
+		    Genode::Vm_handler_base & handler,
+		    Genode::addr_t            ip,
+		    Genode::addr_t            dtb);
+
+		unsigned cpu_id() const   { return _cpu_id;                         }
+		void run()                { if (_active) _vm_session.run(_vcpu_id); }
+		void pause()              { _vm_session.pause(_vcpu_id);            }
+		bool active()             { return _active;                         }
+		State & state() const     { return _state;                          }
+		Gic::Gicd_banked & gic()  { return _gic;                            }
+		void dump();
+
+		template <typename FUNC>
+		void handle_signal(FUNC handler)
+		{
+			if (_active) {
+
+				pause();
+
+				/* check exception reason */
+				switch (_state.exception_type) {
+				case NO_EXCEPTION:                 break;
+				case AARCH64_IRQ:  _handle_irq();  break;
+				case AARCH64_SYNC: _handle_sync(); break;
+				default:
+					throw Exception("Curious exception ",
+					                _state.exception_type, " occured");
+				}
+			}
+
+			handler();
+
+			_update_state();
+
+			if (_active) run();
+		}
+
+		template <typename T>
+		struct Signal_handler : Genode::Vm_handler<Signal_handler<T>>
+		{
+			using Base = Genode::Vm_handler<Signal_handler<T>>;
+
+			Cpu & cpu;
+			T   & obj;
+			void (T::*member)();
+
+			void handle()
+			{
+				try {
+					cpu.handle_signal([this] () { (obj.*member)(); });
+				} catch(Exception &e) {
+					Genode::error(e);
+					cpu.dump();
+				}
+			}
+
+			Signal_handler(Cpu                & cpu,
+			               Genode::Entrypoint & ep,
+			               T                  & o,
+			               void                 (T::*f)())
+			: Base(ep, *this, &Signal_handler::handle),
+			  cpu(cpu), obj(o), member(f) {}
+		};
+};
+
+#endif /* _SRC__SERVER__VMM__CPU_H_ */
