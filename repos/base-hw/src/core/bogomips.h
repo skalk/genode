3134b78c7fb888d002d6eeb1c0e816c8bd414c77
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-12-19 11:04:29 +0100

    fixup

--- repos/base-hw/src/core/bogomips.h
+++ repos/base-hw/src/core/bogomips.h
@@ -1,7 +1,15 @@
-void bogomips() __attribute__((optimize("O0")));
+void bogomips(void);
 
 void bogomips()
 {
-		for (register unsigned i = 0; i < 1000000000; i++) ;
+	asm volatile("str     x19, [sp, #-16]!\n"
+				 "mov     w19, #0x0\n"
+				 "1: mov     w0, #0xc9ff\n"
+				 "movk    w0, #0xff9a, lsl #16\n"
+				 "cmp     w19, w0\n"
+				 "b.hi    2f\n"
+				 "add     w19, w19, #0x1\n"
+				 "b       1b\n"
+				 "2: nop\n"
+				 "ldr     x19, [sp], #16\n" ::: "x0", "x19");
 };
-
