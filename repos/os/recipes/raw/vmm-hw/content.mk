4664c946c7c057c2bce68aba55d963aba53957a5
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2020-01-17 11:33:05 +0100

    depot: recipe for vmm-hw package

---
+++ repos/os/recipes/raw/vmm-hw/content.mk
@@ -0,0 +1,10 @@
+content: dtb initrd linux vmm.config
+
+linux:
+	wget -c -O $@ http://genode.org/files/release-19.11/linux-arm64-image-5.2
+
+initrd:
+	wget -c -O $@ http://genode.org/files/release-19.11/initrd-arm64
+
+vmm.config dtb:
+	cp $(REP_DIR)/recipes/raw/vmm-hw/$@ $@
