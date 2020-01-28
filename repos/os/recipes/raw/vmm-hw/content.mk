278a08643aabf626618553d3772a0466ec17072f
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2020-01-28 15:42:53 +0100

    vmm-hw: change initrd to be used (WIP)

--- repos/os/recipes/raw/vmm-hw/content.mk
+++ repos/os/recipes/raw/vmm-hw/content.mk
@@ -4,7 +4,7 @@ linux:
 	wget -c -O $@ http://genode.org/files/release-19.11/linux-arm64-image-5.2
 
 initrd:
-	wget -c -O $@ http://genode.org/files/release-19.11/initrd-arm64
+	wget -c -O $@ http://genode.org/files/release-19.11/initrd-arm64-rcS
 
 vmm.config dtb:
 	cp $(REP_DIR)/recipes/raw/vmm-hw/$@ $@
