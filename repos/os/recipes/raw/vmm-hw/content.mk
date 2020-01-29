805caa26be3207029bee31e137eeac60902436fe
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2020-01-29 17:27:51 +0100

    vmm-hw: use smaller kernel and initrd

--- repos/os/recipes/raw/vmm-hw/content.mk
+++ repos/os/recipes/raw/vmm-hw/content.mk
@@ -1,10 +1,10 @@
 content: dtb initrd linux vmm.config
 
 linux:
-	wget -c -O $@ http://genode.org/files/release-19.11/linux-arm64-image-5.2
+	wget -c -O $@ http://genode.org/files/release-19.11/linux-arm64-fosdem
 
 initrd:
-	wget -c -O $@ http://genode.org/files/release-19.11/initrd-arm64-rcS
+	wget -c -O $@ http://genode.org/files/release-19.11/initrd-arm64-fosdem
 
 vmm.config dtb:
 	cp $(REP_DIR)/recipes/raw/vmm-hw/$@ $@
